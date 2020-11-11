10184ebbacd72dfe3ff419476ae79b1299f14a9c
Nicola Urlicic <nurlicic@gmail.com>
2020-11-11 12:32:35 +0100

    improved sizing for mobile and removed temp overlay

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ new fullpage("#fullpage", {
   fitToSelection: true,
   paddingTop: "50px",
   paddingBottom: "100px",
+  recordHistory: false,
   onLeave: (origin, destination, direction) => {
     if (!(document.documentElement.clientWidth <= 850 || window.innerWidth <= 850)) {
       const nextSection = destination.item
@@ -44,27 +45,26 @@ new fullpage("#fullpage", {
       }
 
     },
-    anchors: ["main", "toskills", "towork"]
+    anchors: ["main", "skills", "projects"]
 })
 const moveTo = (page) => {
   fullpage_api.moveTo(page, 1);
 }
 
-const toSkills = document.querySelectorAll(".toSkills")
-const toWork = document.querySelectorAll(".toWork")
+const toSkills = document.querySelectorAll(".skills")
+const toWork = document.querySelectorAll(".projects")
 document.querySelector("#home").addEventListener("click", () => moveTo("main"))
 
 
 for (let b=0; b < toSkills.length; b++) {
-  toSkills[b].addEventListener("click", () => moveTo("toskills"))
+  toSkills[b].addEventListener("click", () => moveTo("skills"))
 }
 
 for (let b=0; b < toWork.length; b++) {
-  toWork[b].addEventListener("click", () => moveTo("towork"))
+  toWork[b].addEventListener("click", () => moveTo("projects"))
 }
 
 const tl = new TimelineMax()
-tl.fromTo("#loadOverlay",  { opacity: 1}, { duration: 0.3, x: 0, opacity: 0 })
 tl.fromTo("#hi",  { x: -50, opacity: 0}, { duration: 0.8, x: 0, opacity: 1, ease: "Power2.easeOut"})
 tl.fromTo("#img",  { x: -30, opacity: 0}, { duration: 0.8, x: 0, opacity: 1, ease: "Power2.easeOut"}, "-=0.8")
 tl.fromTo("#title",  { x: 0, opacity: 0}, { duration: 1.2, x: 0, opacity: 1, ease: "Power2.easeOut"})
